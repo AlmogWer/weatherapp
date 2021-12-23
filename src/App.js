f0fd4bea7956067aa6cb1243e6221e902e6eb22f
Almog Wertzberger <87434394+AlmogWer@users.noreply.github.com>
2021-12-23 15:54:41 +0200

    finalizing

--- src/App.js
+++ src/App.js
@@ -53,7 +53,15 @@ function App() {
   };
 
   return (
-    <div className={typeof weather.main !== 'undefined'}>
+    <div
+      className={
+        typeof weather.main !== 'undefined'
+          ? weather.main.temp > 16
+            ? 'app warm'
+            : 'app'
+          : 'app'
+      }
+    >
       <main>
         <div className='search-box'>
           <input
